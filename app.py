6225ecf0e71d3dd80fd00e525be2ade0597a93e2
unknown <j4yn3rd@gmail.com>
2018-05-19 02:03:24 +1200

    testing

--- app.py
+++ app.py
@@ -13,13 +13,11 @@ app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-	req = request.get_json(silent=True, force=True)
-	r = make_response(req)
-	return r
+	return None
 
 
 if __name__ == '__main__':
     # bind to PORT if defined, otherwise default to 5000
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.getenv('PORT', 5000))
     print('Starting Rudy on port %d' % port)
-    app.run(host='0,0,0,0', port=port)
+    app.run(debug=False, port=port, host='0.0.0.0')
