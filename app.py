ea278b5a05c0610a32b65b0311a47d33e4bf0bf8
unknown <j4yn3rd@gmail.com>
2018-05-19 03:52:39 +1200

    testing response

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
 
-import os
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+
 import json
+import os
+import sys
+import logging
 
-from flask import Flask
+from flask import Flask, render_template
 from flask import request
+from flask import make_response
 
-# flask app should start in global layout
+# Flask app should start in global layout
 app = Flask(__name__)
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 
 @app.route('/webhook', methods=['POST'])
@@ -15,11 +24,60 @@ def webhook():
     req = request.get_json(silent=True, force=True)
     print('Request from Dialogflow:')
     print(json.dumps(req, indent=4))
-    print("aaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-    response = json.loads(request.getresponse().read().decode('utf-8'))
-    message = response['result']['fulfillment']['speech']
-    print(message)
-    print("bbbb")
+    res = processRequest(req)
+    res = json.dumps(res, indent=4)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+def processRequest(req):
+    if req.get("result").get("action") != "bookMyConference":
+        return {}
+
+    # oauth
+    orequest = req.get("originalRequest")  # work down the tree
+    odata = orequest.get("data")  # work down the tree
+    user = odata.get("user")  # work down the tree
+    access_token = user.get("access_token")
+
+    # data
+    result = req.get("result")  # work down the tree
+    parameters = result.get("parameters")  # work down the tree
+    startdate = parameters.get("start-date")
+    meetingname = parameters.get("meeting-name")
+
+    payload = {
+        "start-date": startdate,
+        "end-date": startdate,
+        "meeting-name": meetingname
+    }
+
+    # POST info to join.me
+    baseurl = "https://api.join.me/v1/meetings"
+    headers = {
+        'content-type': 'application/json',
+        'authorization': 'Bearer ' + access_token
+    }
+    result = request.post(baseurl, data=json.dumps(payload), headers=headers)
+    data = result.json()
+    print(data)
+    res = makeWebhookResult(data)
+    return res
+
+
+def makeWebhookResult(data):
+    speech = "Appointment scheduled!"
+
+    print("Response:")
+    print(speech)
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+        # "data": data,
+        "source": "heroku-bookmyconference"
+    }
 
 
 if __name__ == '__main__':
