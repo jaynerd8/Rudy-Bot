cbc71294050eed99b8b0f7696f59dccbd020bbf1
unknown <j4yn3rd@gmail.com>
2018-05-19 02:36:34 +1200

    testing

--- app.py
+++ app.py
@@ -13,13 +13,21 @@ app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    req = request.get_json(silent=True, force=True)
-    print('Request from Dialogflow:')
-    print(json.dumps(req, indent=4))
+    speech: str
+    if request.get("result").get("action") == 'getRequisiteForPaper':
+        speech = make_response
+
+    return speech
 
 
+def processRequest(req):
+	result = req.get("result")
+	parameters = result.get("parameters")
+	paper = parameters.get("papers")
+	return paper
+	
 if __name__ == '__main__':
-    # bind to port if defined, otherwise default to 5000
+    # bind to PORT if defined, otherwise default to 5000
     port = int(os.getenv('PORT', 5000))
     print('Starting Rudy on port %d' % port)
-    app.run(port=port, host='0,0,0,0')
+    app.run(debug=False, port=port, host='0.0.0.0')
