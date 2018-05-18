7f024ec557e2592225fdb164911de2a61a65a656
unknown <j4yn3rd@gmail.com>
2018-05-19 02:50:27 +1200

    testing

--- app.py
+++ app.py
@@ -16,7 +16,8 @@ def webhook():
     req = request.get_json(silent=True, force=True)
     print('Request from Dialogflow:')
     print(json.dumps(req, indent=4))
-    return None
+    res = make_response('Awesome')
+    return res
 
 
 if __name__ == '__main__':
