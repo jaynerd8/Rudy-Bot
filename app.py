08dcc3b0fa83d7727da605542a9fc1b498f9dd5f
unknown <j4yn3rd@gmail.com>
2018-05-19 01:03:29 +1200

    webhook test

--- app.py
+++ app.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python
 
 import os
+import json
 
 from flask import Flask
 from flask import request
 from flask import make_response
 
-# Flask app should start in global layout
+# flask app should start in global layout
 app = Flask(__name__)
 
+
 @app.route('/webhook', methods=['POST'])
-def hello():
-    return 'Hello World!'
+def webhook():
+    speech: str
+    if request.get("result").get("action") == 'getRequisiteForPaper':
+        speech = make_response({'fullfillmentText': 'HIHIHIHIHHI'})
+
+    return speech
+
 
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
+    # bind to PORT if defined, otherwise default to 5000
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    print('Starting Rudy on port %d' % port)
+    app.run(debug=False, port=port, host='0,0,0,0')
