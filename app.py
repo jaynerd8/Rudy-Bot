936c51652da4977a00e9b3a92bedd3a0819f368a
unknown <j4yn3rd@gmail.com>
2018-05-20 00:44:32 +1200

    response test

--- app.py
+++ app.py
@@ -3,57 +3,60 @@
 import os
 import json
 
-# flask will be used to interact with Rudy through a predefined webhook
-# jsonify for generating Rudy's response in json format
-# make_response will pass the text response from Rudy to clients
-# request will be client side's request (input) in json format
+# Flask will be used to interact with Rudy through a predefined webhook.
+# 1. jsonify is for translating Rudy's response into an acceptable json
+#    format.
+# 2. make_response will pass the text response from Heroku app to Rudy at
+#    Dialogflow.
+# 3. request contains messages from client side request (input) in json
+#    format
 from flask import Flask, jsonify, make_response, request
 
 app = Flask(__name__)
 
 
-# the fulfillment webhook in Dialogflow should have an ending like=> /webhook
-# For example, if a heroku app has an url of sunnysideup.app, the whole
-# webhook url from Dialogflow to heroku should be sunnysideup.app/webhook
-# so a request should be routed to this webhook function directly
+# The fulfillment webhook in Dialogflow should have an ending of => /webhook.
+# For example, if an heroku app has an url of sunnysideup.app, the whole
+# Heroku's webhook url for Dialogflow should be sunnysideup.app/webhook.
+# Upon a successful connection, a client request should be directly routed to
+# webhook function below.
 @app.route('/webhook', methods=['POST'])
 def webhook():
     # getting a request from rudy
     req = request.get_json(silent=True, force=True)
 
-    # print in json format
+    # print request in json format
     print('Request from Dialogflow:')
     print(json.dumps(req, indent=4))
 
     # process a request
     res = process_request(req)
 
-    # paper = req.get['queryResult']['parameters'].get('papers')
-
-    # paper = req.queryResult.parameters['papers']
+    # return a text response to Rudy for the client request
     return make_response(res)
 
 
+# Forward client's request to an appropriate function based on the intent's
+# action type.
 def process_request(req):
+    res: object
     if req['queryResult'].get('action') == 'getPaperRequisites':
-        paper = req['queryResult']['parameters'].get('paper')
-        print('WOW')
-    res = jsonify({'fulfillmentText': paper})
+        res = get_paper_requisites(req)
     return res
-    # getPaperRequisites(req)
 
 
-def getPaperRequisites(req):
-    result = req.get.queryResult()
-    parameters = result.parameters()
-    paper = parameters['papers']
-    requisites = parameters['requisites']
-    print(paper)
-    print(requisites)
+def get_paper_requisites(req):
+    # result = req.get.queryResult()
+    # parameters = result.parameters()
+    # paper = parameters['papers']
+    paper = req['queryResult']['parameters'].get('paper')
+    requisite = req['queryResult']['parameters'].get('requisite')
+    res = jsonify({'fulfillmentText': paper + requisite})
+    return res
 
 
 if __name__ == '__main__':
-    # bind to port if defined, otherwise default to 5000
+    # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.getenv('PORT', 5000))
     print('Starting Rudy on port %d' % port)
     app.run(debug=False, port=port, host='0.0.0.0')
