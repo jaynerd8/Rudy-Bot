7c452f0525975839f17a86091fe61b0f52f09d17
unknown <j4yn3rd@gmail.com>
2018-05-21 01:10:58 +1200

    testing firebase authentication and retrieve

--- app.py
+++ app.py
@@ -30,16 +30,17 @@ def webhook():
 
 def authenticate():
     key = {
-        os.environ['type'],
-        os.environ['project_id'],
-        os.environ['private_key_id'],
-        os.environ['private_key'],
-        os.environ['client_email'],
-        os.environ['client_id'],
-        os.environ['auth_uri'],
-        os.environ['token_uri'],
-        os.environ['auth_provider_x509_cert_url'],
-        os.environ['client_x509_cert_url']}
+        'type': os.environ['type'],
+        'project_id': os.environ['project_id'],
+        'private_key_id': os.environ['private_key_id'],
+        'private_key': os.environ['private_key'],
+        'client_email': os.environ['client_email'],
+        'client_id': os.environ['client_id'],
+        'auth_uri': os.environ['auth_uri'],
+        'token_uri': os.environ['token_uri'],
+        'auth_provider_x509_cert_url': os.environ['auth_provider_x509_cert_url'],
+        'client_x509_cert_url': os.environ['client_x509_cert_url']}
+
     return key
 
 
