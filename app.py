68f504a7f8f90d293b2118bbb7fd525ec1474339
unknown <j4yn3rd@gmail.com>
2018-05-21 00:56:57 +1200

    testing firebase authentication and retrieve

--- app.py
+++ app.py
@@ -15,29 +15,32 @@ app = Flask(__name__)
 def webhook():
     key = authenticate()
     print(key)
-    # cred = credentials.Certificate(key=authenticate())
-    # firebase = firebase_admin.initialize_app(cred, {'databaseURL': 'https://rudy-b5e54.firebaseio.com'})
+    cred = credentials.Certificate(key)
+    firebase_admin.initialize_app(cred, {'databaseURL': 'https://rudy-b5e54.firebaseio.com'})
 
     req = request.get_json(silent=True, force=True)
     print('Request from client:')
     print(json.dumps(req, indent=4))
 
-    # print(db.reference('degrees').child('Bachelor of Applied Science').get())
+    print(db.reference('degrees').child('Bachelor of Applied Science').get())
 
-    return make_response(req)
+
+# return make_response(req)
 
 
 def authenticate():
-    key = {'type': os.environ['type'],
-           'project_id': os.environ['project_id'],
-           'private_key_id': os.environ['private_key_id'],
-           'private_key': os.environ['private_key'],
-           'client_email': os.environ['client_email'],
-           'client_id': os.environ['client_id'],
-           'auth_uri': os.environ['auth_uri'],
-           'token_uri': os.environ['token_uri'],
-           'auth_provider_x509_cert_url': os.environ['auth_provider_x509_cert_url'],
-           'client_x509_cert_url': os.environ['client_x509_cert_url']}
+    key = {
+        'type': os.environ['type'],
+        'project_id': os.environ['project_id'],
+        'private_key_id': os.environ['private_key_id'],
+        'private_key': os.environ['private_key'],
+        'client_email': os.environ['client_email'],
+        'client_id': os.environ['client_id'],
+        'auth_uri': os.environ['auth_uri'],
+        'token_uri': os.environ['token_uri'],
+        'auth_provider_x509_cert_url': os.environ['auth_provider_x509_cert_url'],
+        'client_x509_cert_url': os.environ['client_x509_cert_url']}
+
     return key
 
 
