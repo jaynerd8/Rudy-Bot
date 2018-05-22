93df1d6be42a11fb30bbe72b6ccb698bccf70ff9
unknown <j4yn3rd@gmail.com>
2018-05-22 22:47:09 +1200

    major details res added

--- app.py
+++ app.py
@@ -1,33 +1,35 @@
 #!/usr/bin/env python
 
-# Importing os to access core libraries of Heroku including secured
-# config vars.
+# ----------------------------------REGION IMPORT---------------------------------- #
+
+# Importing os to access core libraries of Heroku including secured config vars.
 import os
 
-# Importing json to manage communication contents in the acceptable
-# json format.
+# Importing json to manage communication contents in the acceptable json format.
 import json
 
-# Importing firebase_admin to authenticate server's Firebase database
-# access.
+# Importing firebase_admin to authenticate server's Firebase database access.
 import firebase_admin
 
-# credentials will be used to confirm identification of the service
-# key provided in Heroku's config vars.
+# credentials will be used to confirm identification of the service key provided in
+# Heroku's config vars.
 from firebase_admin import credentials
 
 # db will be the main database that Rudy will be accessing.
 from firebase_admin import db
 
-# Flask package will manage flow of communications between the server
-# and client.
-# 1. jsonify will be used for translating Rudy's response into
-# an acceptable json format.
-# 2. make_response will pass the text response
-# from Heroku app (Rudy) to client's Dialogflow view.
-# 3. request components contains json format messages from the client
-# side (input).
+# Flask package will manage flow of communications between the server and client.
+# 1. jsonify will be used for translating Rudy's response into an acceptable json
+# format.
+# 2. make_response will pass the text response from Heroku app (Rudy) to client's
+# Dialogflow view.
+# 3. request components contains json format messages from the client side (input).
 from flask import Flask, jsonify, make_response, request
+
+# --------------------------------END_REGION IMPORT-------------------------------- #
+
+
+# ----------------------------------REGION GLOBAL---------------------------------- #
 
 # Starting flask app in global layout.
 app = Flask(__name__)
@@ -56,14 +58,19 @@ print('Rudy (Firebase): Firebase access granted.')
 
 # Generating database references.
 db_requisites = db.reference('requisites')
+db_majors = db.reference('majors')
 
 
-# The fulfillment webhook settings in Dialogflow should have a url
-# that ends with '/webhook'. For example, if an Heroku app has a url of
-# rudybot.app, the finalized Dialogflow's fulfillment webhook integration
-# address should be 'rudybot.app/webhook'. Upon generating a successful
-# server-client connection, the client request will be routed to the
-# webhook function below.
+# --------------------------------END_REGION GLOBAL-------------------------------- #
+
+
+# ---------------------------------REGION WEBHOOK---------------------------------- #
+
+# The fulfillment webhook settings in Dialogflow should have a url that ends with
+# '/webhook'. For example, if an Heroku app has a url of rudybot.app, the finalized
+# Dialogflow's webhook integration address should be 'rudybot.app/webhook'. Upon
+# generating a successful server-client connection, the client request will be rout
+# -ed to the webhook function below.
 @app.route('/webhook', methods=['POST'])
 def webhook():
     # Get a request from client then print.
@@ -74,13 +81,12 @@ def webhook():
     # Process the request to get a response.
     res = process_request(req)
 
-    # Returning acquired response in json format to the client's
-    # Dialogflow view.
+    # Returning acquired response in json format to the client's Dialogflow view.
     return make_response(res)
 
 
-# Sorting out client's request then forward to a matching function based
-# on the action parameter of the request's intent.
+# Sorting out client's request then forward to a matching function based on the
+# action parameter of the request's intent.
 def process_request(req):
     print('Rudy (Heroku): Request processing started.')
 
@@ -88,6 +94,8 @@ def process_request(req):
     result: str
     if req['queryResult'].get('action') == 'getPaperRequisites':
         result = get_paper_requisites(req)
+    elif req['queryResult'].get('action') == 'getMajorDetails':
+        result = get_major_details(req)
 
     # Show the response log before jsonify.
     print('Rudy (Heroku): Generated response ->')
@@ -97,9 +105,15 @@ def process_request(req):
     return jsonify({'fulfillmentText': result})
 
 
-# Get required parameters from the request for a database query.
+# -------------------------------END_REGION WEBHOOK-------------------------------- #
+
+
+# --------------------------------REGION PARAMETERS-------------------------------- #
+
+# Get required parameters (papers & requirements) from the request for a database
+# query.
 def get_paper_requisites(req):
-    print('Rudy (Flask): Extracting required parameters.')
+    print('Rudy (Flask): Extracting required parameters. (papers)')
 
     # Getting parameters.
     speech = ''
@@ -116,15 +130,51 @@ def get_paper_requisites(req):
     for result in requisites_query:
         if result is None:
             print('Rudy (Firebase): Requisites query is empty.')
-            speech += 'There are no ' + requisites[counter] + ' for paper: ' + paper + ". "
+            speech += 'There are no ' + requisites[counter] + ' for paper: ' \
+                      + paper + '. '
         else:
             print('Rudy (Firebase): Parsing query results.')
-            speech += 'The list of ' + requisites[counter] + ' are: ' + str(result).strip('[]') + ". "
+            speech += 'The list of ' + requisites[counter] + ' are: ' \
+                      + str(result).strip('[]') + '. '
         counter += 1
 
     # Returning the speech contexts.
     return speech
 
+
+# Get required parameters (majors) from the request for a database query.
+def get_major_details(req):
+    print('Rudy (Flask): Extracting required parameters. (majors)')
+
+    # Getting parameters.
+    speech = ''
+    major = req['queryResult']['parameters'].get('major')
+
+    # Query creation.
+    print('Rudy (Heroku): Major details query created.')
+    details_query = make_details_query(major)
+
+    # Parsing query results into a speech format.
+    counter = 1
+    for result in details_query:
+        if result is None:
+            print('Rudy (Firebase): Major details query is empty.')
+            speech += 'There are no Year ' + counter + ' courses for ' + major \
+                      + ' major. '
+        else:
+            print('Rudy (Firebase): Parsing query results.')
+            speech += 'The list of suggested courses for Year ' + counter + ' are: ' \
+                      + str(result).strip('[]') + '. '
+        counter += 1
+
+    # Returning the speech contexts.
+    return speech
+
+
+# ------------------------------END_REGION PARAMETERS------------------------------ #
+
+
+# -----------------------------------REGION QUERY---------------------------------- #
 
 # Get requisite data source from Firebase based on the request parameters.
 def make_requisites_query(paper, requisites):
@@ -141,10 +191,28 @@ def make_requisites_query(paper, requisites):
     return query_result
 
 
+# Get major details data source from Firebase based on the request parameters.
+def make_details_query(major):
+    print('Rudy (Firebase): Accessing to the database.')
+
+    # Making a list of query results for multiple year details for the major.
+    query_result = []
+    first_year = [db_majors.child(major).child('Year1').get()]
+    second_year = [db_majors.child(major).child('Year2').get()]
+    third_year = [db_majors.child(major).child('Year3').get()]
+    query_result.append(first_year, second_year, third_year)
+
+    # Returning collected query results.
+    return query_result
+
+
+# ---------------------------------END_REGION QUERY-------------------------------- #
+
+
 # Initializing the app to server connection (hosting).
 if __name__ == '__main__':
-    # Setting the default port to 5000. Other defined values can be
-    # used as an alternative.
+    # Setting the default port to 5000. Other defined values can be used as an
+    # alternative.
     port = int(os.getenv('PORT', 5000))
     print('Rudy (Heroku): Starting Rudy on port %d' % port)
     app.run(debug=True, port=port, host='0.0.0.0')
